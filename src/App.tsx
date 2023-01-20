7da947aef8ef2b1e3ae358d522f98f20fec179b2
Luis Maracara <luis.maracara1@gmail.com>
2023-01-20 11:11:49 -0300

    Add Material UI framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Button from '@mui/material/Button'
 import logo from './logo.svg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './App.css';
 
 function App() {
@@ -10,14 +15,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Button variant='contained' href='https://reactjs.org' target={'_blank'} rel='noopener noreferrer'>
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
   );
